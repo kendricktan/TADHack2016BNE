d338b9c3236b9174f5fa8d162fae239b9b9170fc
kendricktan <kendricktan0814@gmail.com>
2016-10-15 20:12:54 +1000

    .

--- drone/cisco_bot.js
+++ drone/cisco_bot.js
@@ -10,23 +10,38 @@ app.use(bodyParser.json());
 
 var rollingSpider = new RollingSpider();
 
-// Start our parrot
+// start express server
+var server = app.listen(8080, function () {
+    console.log('on port 8080')
+});
 
+function bot_reply(txt){
+    request.post({
+        url: 'https://api.ciscospark.com/v1/messages',
+        form: {
+            'roomId': '728c2ea0-9299-11e6-b930-5b92f116b331',
+            'text': txt
+        },
+        headers:{
+            'Authorization': 'Bearer YzliZDlmZmMtN2FiOC00ODA1LTlmZDItNDQ4MGQxMTE4NzljOWJjODliODQtMGIw',
+        },
+        function (error, response, body){
+        }
+    });
+
+}
+
+// Start our parrot controller
 rollingSpider.connect(function(){
-    console.log('Connecting to drone!')
+    bot_reply('Attempting to connect to drone...')
     rollingSpider.setup(function(){
-        console.log('Connected!')
+        bot_reply('Successfully connected to drone!')
         rollingSpider.flatTrim();
         rollingSpider.startPing();
         rollingSpider.flatTrim();
     })
 });
 
-
-// start express server
-var server = app.listen(8080, function () {
-    console.log('on port 8080')
-});
 
 // define express path for incoming webhooks
 // Someone posted a message, lets get it
@@ -44,6 +59,11 @@ app.post('/', function(req, resp){
             var data = JSON.parse(body);
             var key_words = data['text'];
 
+            // Ignore commands from drones@sparkbot.io
+            if (data['personEmail'].indexOf('drones@sparkbot.io') != -1){
+                return
+            }
+
             // If '/fly' then fly
             if (key_words.indexOf('/fly') != -1){
                 rollingSpider.takeOff();
@@ -58,14 +78,38 @@ app.post('/', function(req, resp){
             }
 
             else if (key_words.indexOf('/forward') != -1){
-                rollingSpider.forward(50, 15);
+                rollingSpider.forward({'speed': 50, 'steps': 20});
+                rollingSpider.flatTrim();
                 console.log('going forwards...');
             }
 
             else if (key_words.indexOf('/backward') != -1){
-                rollingSpider.backward(50, 15);
+                rollingSpider.backward({'speed':50, 'steps': 20});
+                rollingSpider.flatTrim();
                 console.log('going backwards...');
+            }
+
+            else if (key_words.indexOf('/up') != -1){
+                rollingSpider.up({'speed': 50, 'steps': 20});
+                rollingSpider.flatTrim();
+                console.log('going up...')
+            }
+
+            else if (key_words.indexOf('/down') != -1){
+                rollingSpider.down({'speed': 50, 'steps': 20});
+                rollingSpider.flatTrim();
+                console.log('going down...')
+            }
+
+            else if (key_words.indexOf('/help') != -1){
+                console.log('help...')
+                bot_reply('/fly - flies the drone\n/land - lands the drone\n/forward - moves the drone forwards\n/backward - moves the drone backwards\n/up - files the drone higher\n/down - files the drone lower')
+            }
+
+            else{
+                bot_reply('unknown command, please look up /help')
             }
         }
     );
+
 });
